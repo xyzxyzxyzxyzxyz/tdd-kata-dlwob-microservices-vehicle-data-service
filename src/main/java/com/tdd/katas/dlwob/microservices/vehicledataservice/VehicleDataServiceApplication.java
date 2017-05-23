be6d5021f5597c0697437525617f3289006935a2
XYZ <idm.caz@mailinator.com>
2017-05-23 12:06:49 +0200

    Initial commit

---
+++ src/main/java/com/tdd/katas/dlwob/microservices/vehicledataservice/VehicleDataServiceApplication.java
@@ -0,0 +1,12 @@
+package com.tdd.katas.dlwob.microservices.vehicledataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleDataServiceApplication.class, args);
+	}
+}
